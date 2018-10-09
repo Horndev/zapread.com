4e88baf27b93e996a346db2127ed30506df8b9c1
Steven Horn <horn@horndev.com>
2018-10-09 19:21:44 -0300

    Adding some content and test project.

---
+++ zapread.com.Tests/Controllers/VoteControllerTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace zapread.com.Tests.Controllers
+{
+    [TestClass]
+    public class VoteControllerTests
+    {
+        [TestMethod]
+        public void TestVoteAsUser()
+        {
+
+        }
+    }
+}
