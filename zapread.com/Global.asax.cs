5d493d5fa0af05ca5f2f91dc00068445d994703d
Steven Horn <horn@horndev.com>
2018-12-18 21:23:25 -0400

    Added force for https when deployed

--- zapread.com/Global.asax.cs
+++ zapread.com/Global.asax.cs
@@ -19,6 +19,20 @@ namespace zapread.com
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
-        
+
+        protected void Application_BeginRequest()
+        {
+            if (!Context.Request.IsSecureConnection)
+            {
+                // This is an insecure connection, so redirect to the secure version
+                UriBuilder uri = new UriBuilder(Context.Request.Url);
+                if (!uri.Host.Equals("localhost"))
+                {
+                    uri.Port = 443;
+                    uri.Scheme = "https";
+                    Response.Redirect(uri.ToString());
+                }
+            }
+        }
     }
 }
