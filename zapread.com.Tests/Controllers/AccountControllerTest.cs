097d04896ae28d5ed26ae2b03d7d77b8cae5d051
Steven Horn <horn@horndev.com>
2018-12-29 19:04:17 -0400

    Added unit test for account GetBalance with mock http request

--- zapread.com.Tests/Controllers/AccountControllerTest.cs
+++ zapread.com.Tests/Controllers/AccountControllerTest.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using zapread.com.Controllers;
 
 namespace zapread.com.Tests.Controllers
@@ -9,7 +12,7 @@ namespace zapread.com.Tests.Controllers
     public class AccountControllerTest
     {
         [TestMethod]
-        public void TestBalance()
+        public void TestAccountBalance()
         {
             // Arrange
             AccountController controller = new AccountController();
@@ -20,5 +23,31 @@ namespace zapread.com.Tests.Controllers
             // Assert
             Assert.IsNotNull(result);
         }
+
+        [TestMethod]
+        public void TestAccountGetBalanceUnauthenticated()
+        {
+            var request = new Mock<HttpRequestBase>();
+
+            request.SetupGet(x => x.Headers).Returns(
+                new System.Net.WebHeaderCollection {
+                    {"X-Requested-With", "XMLHttpRequest"}
+                });
+
+            var context = new Mock<HttpContextBase>();
+            context.SetupGet(x => x.Request).Returns(request.Object);
+
+            // Arrange
+            AccountController controller = new AccountController();
+            controller.ControllerContext = new ControllerContext(context.Object, new RouteData(), controller);
+
+            // Act
+            JsonResult result = controller.GetBalance() as JsonResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+
     }
 }
