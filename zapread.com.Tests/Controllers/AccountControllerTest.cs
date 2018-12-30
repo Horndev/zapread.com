d9a16745bddd81ea9d58890b765ea6b19c228505
Steven Horn <horn@horndev.com>
2018-12-29 21:45:24 -0400

    Updated missing refs to Owin 3.1.0

--- zapread.com.Tests/Controllers/AccountControllerTest.cs
+++ zapread.com.Tests/Controllers/AccountControllerTest.cs
@@ -118,43 +118,6 @@ namespace zapread.com.Tests.Controllers
             // Assert
             Assert.IsNotNull(result);
         }
-
     }
-
-    public class MockUserStore : IUserStore<ApplicationUser>
-    {
-        public MockUserStore()
-        {
-        }
-
-        public Task CreateAsync(ApplicationUser user)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task DeleteAsync(ApplicationUser user)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Dispose()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<ApplicationUser> FindByIdAsync(string userId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<ApplicationUser> FindByNameAsync(string userName)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task UpdateAsync(ApplicationUser user)
-        {
-            throw new NotImplementedException();
-        }
-    }
+    
 }
