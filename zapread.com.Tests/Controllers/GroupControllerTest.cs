68ead42d000087cd46ec9e96bbabc0eeafb02014
Steven Horn <horn@horndev.com>
2018-12-31 12:10:34 -0400

    Added test for group index.

--- zapread.com.Tests/Controllers/GroupControllerTest.cs
+++ zapread.com.Tests/Controllers/GroupControllerTest.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using zapread.com.Controllers;
 
 namespace zapread.com.Tests.Controllers
@@ -16,6 +21,27 @@ namespace zapread.com.Tests.Controllers
 
             // Act
             ViewResult result = controller.Members(1) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void TestGroupIndex()
+        {
+            // Arrange
+            var context = new Mock<HttpContextBase>();
+
+            var identity = new GenericIdentity("test");
+            identity.AddClaim(new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier", "f752739e-8d58-4bf5-a140-fc225cc5ebdb")); //test user
+            var principal = new GenericPrincipal(identity, new[] { "user" });
+            context.Setup(s => s.User).Returns(principal);
+
+            GroupController controller = new GroupController();
+            controller.ControllerContext = new ControllerContext(context.Object, new RouteData(), controller);
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
 
             // Assert
             Assert.IsNotNull(result);
