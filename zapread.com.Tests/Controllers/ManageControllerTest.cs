f8bfd84bd8def0a65804d413f140b9e9db55273a
Steven Horn <horn@horndev.com>
2019-01-01 19:54:54 -0400

    Added unit test for ManageController Index

---
+++ zapread.com.Tests/Controllers/ManageControllerTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using zapread.com.Controllers;
+using zapread.com.Models;
+
+namespace zapread.com.Tests.Controllers
+{
+    [TestClass]
+    public class ManageControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            var context = new Mock<HttpContextBase>();
+
+            var identity = new GenericIdentity("test");
+            identity.AddClaim(new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier", "f752739e-8d58-4bf5-a140-fc225cc5ebdb")); //test user
+            var principal = new GenericPrincipal(identity, new[] { "user" });
+            context.Setup(s => s.User).Returns(principal);
+
+            var userStore = new Mock<IUserStore<ApplicationUser>>();
+            var userManager = new Mock<ApplicationUserManager>(userStore.Object);
+            var authenticationManager = new Mock<IAuthenticationManager>();
+            var signInManager = new Mock<ApplicationSignInManager>(userManager.Object, authenticationManager.Object);
+
+            var claimsIdentity = new Mock<ClaimsIdentity>(MockBehavior.Loose);
+
+            claimsIdentity.Setup(x => x.AddClaim(It.IsAny<Claim>()));
+
+            IList<UserLoginInfo> userlogins = new List<UserLoginInfo>();
+
+            userManager.Setup(x => x.GetPhoneNumberAsync(It.IsAny<string>())).Returns(Task.FromResult("123"));
+            userManager.Setup(x => x.GetTwoFactorEnabledAsync(It.IsAny<string>())).Returns(Task.FromResult(true));
+            userManager.Setup(x => x.GetLoginsAsync(It.IsAny<string>())).Returns(Task.FromResult(userlogins));
+
+            ManageController controller = new ManageController(userManager.Object, signInManager.Object);
+            controller.ControllerContext = new ControllerContext(context.Object, new RouteData(), controller);
+
+            // Act
+            ViewResult result = controller.Index(null).Result as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
--- zapread.com/Controllers/ManageController.cs
+++ zapread.com/Controllers/ManageController.cs
@@ -591,7 +591,7 @@ namespace zapread.com.Controllers
                     PhoneNumber = await UserManager.GetPhoneNumberAsync(userId),
                     TwoFactor = await UserManager.GetTwoFactorEnabledAsync(userId),
                     Logins = await UserManager.GetLoginsAsync(userId),
-                    BrowserRemembered = await AuthenticationManager.TwoFactorBrowserRememberedAsync(userId),
+                    //BrowserRemembered = await AuthenticationManager.TwoFactorBrowserRememberedAsync(userId),
                     AboutMe = new AboutMeViewModel() { AboutMe = aboutMe },
                     Financial = new FinancialViewModel() { Transactions = txnView, Earnings = earningsView, Spendings = spendingsView },
                     UserGroups = new ManageUserGroupsViewModel() { Groups = gi },
