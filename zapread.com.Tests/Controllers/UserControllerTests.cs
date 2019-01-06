9506d3544b6514a40a20c7d33bac3b2711d6812f
Steven Horn <horn@horndev.com>
2019-01-05 20:30:39 -0400

    Added UserControllerTests, added ToggleIgnore Test

---
+++ zapread.com.Tests/Controllers/UserControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using zapread.com.Controllers;
+
+namespace zapread.com.Tests.Controllers
+{
+    [TestClass]
+    public class UserControllerTests
+    {
+        [TestMethod]
+        public void TestUserToggleIgnore()
+        {
+            // Arrange
+            var context = new Mock<HttpContextBase>();
+
+            var identity = new GenericIdentity("test");
+            identity.AddClaim(new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier", "f752739e-8d58-4bf5-a140-fc225cc5ebdb")); //test user
+            var principal = new GenericPrincipal(identity, new[] { "user" });
+            context.Setup(s => s.User).Returns(principal);
+
+            UserController controller = new UserController();
+            controller.ControllerContext = new ControllerContext(context.Object, new RouteData(), controller);
+
+            // Act
+            JsonResult result = controller.ToggleIgnore(1) as JsonResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+
+        }
+    }
+}
