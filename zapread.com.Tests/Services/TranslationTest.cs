759ef04745e74d05231d8c2c7098f169471b125a
Steven Horn <horn@horndev.com>
2019-01-07 21:40:54 -0400

    Added unit test stub for google cloud translation

---
+++ zapread.com.Tests/Services/TranslationTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Google.Cloud.Translation.V2;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Translate.v2;
+
+namespace zapread.com.Tests.Services
+{
+    [TestClass]
+    public class TranslationTest
+    {
+        [TestMethod]
+        public void TestDetectLanguage()
+        {
+            //Assert.Inconclusive();
+            //var c = new BaseClientService.Initializer ()
+            //{
+                
+            //    ApplicationName = "ZapRead",
+            //    ApiKey = "",  // IP Restricted, API restricted
+            //};
+
+            //var service = new TranslateService(c);
+
+            //TranslationClient client = new TranslationClientImpl(service, TranslationModel.ServiceDefault);
+            //var detection = client.DetectLanguage(text: "Hello world.");
+            //Console.WriteLine(
+            //    $"{detection.Language}\tConfidence: {detection.Confidence}");
+        }
+    }
+}
