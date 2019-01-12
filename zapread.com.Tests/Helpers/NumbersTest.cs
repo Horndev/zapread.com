db144da4902db5183872591e8cb14baf124bfcd2
Steven Horn <horn@horndev.com>
2019-01-12 15:19:39 -0400

    Fix bug in negative abbreviated numbers, added unit test.

---
+++ zapread.com.Tests/Helpers/NumbersTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using zapread.com.Helpers;
+
+namespace zapread.com.Tests.Helpers
+{
+    [TestClass]
+    public class NumbersTest
+    {
+        [TestMethod]
+        public void TestAbbr()
+        {
+            double LargePostive = 123456789.0;
+            double LargeNegative = -95421351.0;
+
+            string LP = LargePostive.ToAbbrString();
+            string LN = LargeNegative.ToAbbrString();
+
+            Assert.AreEqual("123.5M", LP);
+            Assert.AreEqual("-95.4M", LN);
+        }
+    }
+}
--- zapread.com/Helpers/NumberHelpers.cs
+++ zapread.com/Helpers/NumberHelpers.cs
@@ -7,29 +7,27 @@ namespace zapread.com.Helpers
 {
     public static class NumberHelpers
     {
-        public static String ToAbbrString(this double number)
+        public static string ToAbbrString(this double number)
         {
-            String ret = Convert.ToString(number);
-            if (number > 1000000000)
+            string ret = Convert.ToString(number);
+            if (Math.Abs(number) > 1000000000)
                 ret = (number / 1000000000.0).ToString("0.#") + "G";
-            else if (number > 1000000)
+            else if (Math.Abs(number) > 1000000)
                 ret = (number / 1000000.0).ToString("0.#") + "M";
-            else if (number > 1000)
+            else if (Math.Abs(number) > 1000)
                 ret = (number / 1000.0).ToString("0.#") + "K";
-
             return ret;
         }
 
-        public static String ToAbbrString(this int number)
+        public static string ToAbbrString(this int number)
         {
-            String ret = Convert.ToString(number);
-            if (number > 1000000000)
+            string ret = Convert.ToString(number);
+            if (Math.Abs(number) > 1000000000)
                 ret = (number / 1000000000.0).ToString("0.#") + "G";
-            else if (number > 1000000)
+            else if (Math.Abs(number) > 1000000)
                 ret = (number / 1000000.0).ToString("0.#") + "M";
-            else if (number > 1000)
+            else if (Math.Abs(number) > 1000)
                 ret = (number / 1000.0).ToString("0.#") + "K";
-
             return ret;
         }
     }
