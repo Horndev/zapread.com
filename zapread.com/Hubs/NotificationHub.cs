b839a28f74834466824d0dd09c18f0d987c8d2f8
Steven Horn <horn@horndev.com>
2019-01-12 23:09:22 -0400

    Adding user-based real time notifications.

--- zapread.com/Hubs/NotificationHub.cs
+++ zapread.com/Hubs/NotificationHub.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
+using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.SignalR;
 
 namespace zapread.com.Hubs
@@ -13,9 +15,29 @@ namespace zapread.com.Hubs
             Clients.All.hello();
         }
 
+        // Send notification to all users
         public void NotifyInvoicePaid(string invoice)
         {
             Clients.All.notifyInvoicePaid(invoice);
+        }
+
+        // Send notification only to user
+        public void NotifyInvoicePaid(string invoice, string userId)
+        {
+            //Clients.All.notifyInvoicePaid(invoice);
+            Clients.Group(userId).notifyInvoicePaid(invoice);
+        }
+
+        public override Task OnConnected()
+        {
+            string name = Context.User.Identity.GetUserId();//.Name;
+
+            if (name != null)
+            {
+                Groups.Add(Context.ConnectionId, name);
+            }
+            
+            return base.OnConnected();
         }
     }
 }
