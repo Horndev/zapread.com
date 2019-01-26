d37554e7532840e692b6d1a4cb9ba6d4a5879569
Steven Horn <horn@horndev.com>
2019-01-26 11:03:34 -0400

    Added UserController Index test.  Updated TestAdapter to version 1.4.0

--- zapread.com.Tests/Controllers/UserControllerTests.cs
+++ zapread.com.Tests/Controllers/UserControllerTests.cs
@@ -14,6 +14,27 @@ namespace zapread.com.Tests.Controllers
     public class UserControllerTests
     {
         [TestMethod]
+        public void UserIndex()
+        {
+            // Arrange
+            var context = new Mock<HttpContextBase>();
+
+            var identity = new GenericIdentity("test");
+            identity.AddClaim(new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier", "f752739e-8d58-4bf5-a140-fc225cc5ebdb")); //test user
+            var principal = new GenericPrincipal(identity, new[] { "user" });
+            context.Setup(s => s.User).Returns(principal);
+
+            UserController controller = new UserController();
+            controller.ControllerContext = new ControllerContext(context.Object, new RouteData(), controller);
+
+            // Act
+            ViewResult result = controller.Index(username: "Jimbob") as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
         public void TestUserToggleIgnore()
         {
             // Arrange
