ca7c66aa19e05e45089af8a8daeb8cfc42cfb0f2
Steven Horn <horn@horndev.com>
2019-02-03 23:31:50 -0400

    Mailer for posts working - needs tweaks.

--- zapread.com/Controllers/MailerController.cs
+++ zapread.com/Controllers/MailerController.cs
@@ -5,8 +5,11 @@ using System.Linq;
 using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using zapread.com.Database;
 using zapread.com.Models;
 using zapread.com.Services;
+using System.Data.Entity;
+using HtmlAgilityPack;
 
 namespace zapread.com.Controllers
 {
@@ -22,10 +25,47 @@ namespace zapread.com.Controllers
         /// Mailer renders HTML for new Post.
         /// </summary>
         /// <returns></returns>
-        public ActionResult NewPost()
+        public ActionResult NewPost(int id)
         {
+            using (var db = new ZapContext())
+            {
 
-            return View();
+                var pst = db.Posts
+                    .Include(p => p.Group)
+                    .Include(p => p.Comments)
+                    .Include(p => p.Comments.Select(cmt => cmt.Parent))
+                    .Include(p => p.Comments.Select(cmt => cmt.VotesUp))
+                    .Include(p => p.Comments.Select(cmt => cmt.VotesDown))
+                    .Include(p => p.Comments.Select(cmt => cmt.UserId))
+                    .Include("UserId")
+                    .AsNoTracking()
+                    .FirstOrDefault(p => p.PostId == id);
+
+                List<int> viewerIgnoredUsers = new List<int>();
+                
+                if (pst == null)
+                {
+                    return RedirectToAction("PostNotFound");
+                }
+
+                var groups = db.Groups
+                        .Select(gr => new { gr.GroupId, pc = gr.Posts.Count, mc = gr.Members.Count, l = gr.Tier })
+                        .AsNoTracking()
+                        .ToList();
+
+                PostViewModel vm = new PostViewModel()
+                {
+                    Post = pst,
+                };
+
+                string HTMLString = RenderViewToString("NewPost", vm);
+
+                PreMailer.Net.InlineResult result;
+                string msgHTML;
+                SendTestMail(HTMLString, out result, out msgHTML);
+
+                return View(vm);
+            }
         }
 
         [HttpGet, AllowAnonymous]
@@ -33,21 +73,58 @@ namespace zapread.com.Controllers
         {
             string HTMLString = RenderViewToString("Index", null);
 
-            var baseUri = new Uri("https://www.zapread.com");
-            var pm = new PreMailer.Net.PreMailer(HTMLString, baseUri);
+            //var baseUri = new Uri("https://www.zapread.com");
+            //var pm = new PreMailer.Net.PreMailer(HTMLString, baseUri);
 
-            var result = pm.MoveCssInline();
-                //PreMailer.Net.PreMailer.MoveCssInline(
-                //    baseUri: new Uri("https://www.zapread.com"),
-                //    html: HTMLString,
-                //    removeComments: true
-                //    );
+            PreMailer.Net.InlineResult result;
+            string msgHTML;
+            SendTestMail(HTMLString, out result, out msgHTML);
 
-            var msgHTML = result.Html;      // Resultant HTML, with CSS in-lined.
             var msgWarn = String.Join(",", result.Warnings);    // string[] of any warnings that occurred during processing.
-
             Encoding utf8 = Encoding.UTF8;
             Encoding ascii = Encoding.ASCII;
+            return Json(new
+            {
+                HTMLString,
+                msg = ascii.GetString(Encoding.Convert(utf8, ascii, utf8.GetBytes(msgHTML))),
+                msgWarn
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        private static void SendTestMail(string HTMLString, out PreMailer.Net.InlineResult result, out string msgHTML)
+        {
+            var baseUri = new Uri("https://www.zapread.com/");
+            result = PreMailer.Net.PreMailer.MoveCssInline(
+                                baseUri: baseUri,
+                                html: HTMLString,
+                                removeComments: true,
+                                stripIdAndClassAttributes: true
+                                );
+            string msgHTMLPre = result.Html;
+
+            var doc = new HtmlDocument();
+            doc.LoadHtml(msgHTMLPre);
+            var imgs = doc.DocumentNode.SelectNodes("//img/@src");
+            if (imgs != null)
+            {
+                foreach (var item in imgs)
+                {
+                    // TODO: check if external url
+                    item.SetAttributeValue("src", new Uri(baseUri, item.GetAttributeValue("src", "")).AbsoluteUri);
+                }
+            }
+
+            var links = doc.DocumentNode.SelectNodes("//a/@href");
+            if (links != null)
+            {
+                foreach (var link in links)
+                {
+                    // TODO: check if external url
+                    link.SetAttributeValue("href", new Uri(baseUri, link.GetAttributeValue("href", "")).AbsoluteUri);
+                }
+            }
+
+            msgHTML = doc.DocumentNode.OuterHtml;
 
             MailingService.Send(new UserEmailModel()
             {
@@ -57,13 +134,6 @@ namespace zapread.com.Controllers
                 Name = "zapread.com Mailer",
                 Subject = "zapread.com Mailer Test",
             });
-
-            return Json(new
-            {
-                HTMLString,
-                msg = ascii.GetString(Encoding.Convert(utf8, ascii, utf8.GetBytes(msgHTML))),
-                msgWarn
-            }, JsonRequestBehavior.AllowGet);
         }
 
         protected string RenderPartialViewToString(string viewName, object model)
