86a3c7485611df5e4faf56d20bbbb7c259c78234
Steven Horn <horn@horndev.com>
2019-03-05 23:03:18 -0400

    Minor refactoring of post-initialize

--- zapread.com/Scripts/Posts/post-ui.js
+++ zapread.com/Scripts/Posts/post-ui.js
@@ -1,6 +1,11 @@
 ﻿
 var toggleComment = function (e) {
     $(e).parent().find('.comment-body').first().fadeToggle({ duration: 0 });
+
+    //$(e).parent().find('.comment-body').each(function (index, item) {
+    //    $(item).show();
+    //});
+
     if ($(e).find('.togglebutton').hasClass("fa-minus-square")) {
         $(e).removeClass('pull-left');
         $(e).addClass('commentCollapsed');
