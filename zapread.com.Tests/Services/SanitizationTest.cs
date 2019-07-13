75f80643352c9cfb2cae5dd78e376f4af8b503c6
Steven Horn <horn@horndev.com>
2019-07-12 22:19:53 -0300

    Fixes for eager stripping of UTF characters in sanitization.

---
+++ zapread.com.Tests/Services/SanitizationTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using zapread.com.Controllers;
+
+namespace zapread.com.Tests.Services
+{
+    [TestClass]
+    public class SanitizationTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var text = @"<script>window.location.href = 'https://www.google.com';</script>";
+
+            var clean = PostController.SanitizePostXSS(text);
+
+            Assert.IsTrue(clean == "");
+
+            var text2 = @"Ś ę ę ć ż ł ł ł ó ż ł ó ż ć";
+
+            var clean2 = PostController.SanitizePostXSS(text2);
+
+            Assert.IsTrue(clean2 != "");
+        }
+    }
+}
