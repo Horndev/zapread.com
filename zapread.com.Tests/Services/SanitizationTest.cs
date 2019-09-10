d1b6bb0989bc3f3244733f0f00e39e50398d4461
Steven Horn <horn@horndev.com>
2019-09-09 22:00:34 -0300

    Fix unit test for Sanitization

--- zapread.com.Tests/Services/SanitizationTest.cs
+++ zapread.com.Tests/Services/SanitizationTest.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using zapread.com.Controllers;
+using zapread.com.Helpers;
 
 namespace zapread.com.Tests.Services
 {
@@ -11,13 +12,13 @@ namespace zapread.com.Tests.Services
         {
             var text = @"<script>window.location.href = 'https://www.google.com';</script>";
 
-            var clean = PostController.SanitizePostXSS(text);
+            var clean = text.SanitizeXSS();
 
             Assert.IsTrue(clean == "");
 
             var text2 = @"Ś ę ę ć ż ł ł ł ó ż ł ó ż ć";
 
-            var clean2 = PostController.SanitizePostXSS(text2);
+            var clean2 = text2.SanitizeXSS();
 
             Assert.IsTrue(clean2 != "");
         }
