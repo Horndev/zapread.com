7adca662c569f5fb3a4d19f53c271978fae111af
Steven Horn <horn@horndev.com>
2019-09-18 23:23:49 -0300

    Add in info for AchievementsViewModel

--- zapread.com/Models/Admin/AdminAchievementsViewModel.cs
+++ zapread.com/Models/Admin/AdminAchievementsViewModel.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using zapread.com.Models.Database;
 
 namespace zapread.com.Models.Admin
 {
     public class AdminAchievementsViewModel
     {
-
+        public List<Achievement> Achievements { get; set; }
     }
 }
