484759fca174a84dced1757592f3f0f660790d89
Steven Horn <horn@horndev.com>
2019-09-26 22:44:30 -0300

    Toggle comments with post.

--- zapread.com/Scripts/Posts/post-ui.js
+++ zapread.com/Scripts/Posts/post-ui.js
@@ -1,19 +1,20 @@
-﻿
-var toggleComment = function (e) {
+﻿/**/
+
+/**
+ * Toggle the comment and it's children
+ * @param {any} e element of close button
+ * @param {any} d direction force (1 force close, -1 force open, 0 toggle)
+ */
+var toggleComment = function (e, d) {
     $(e).parent().find('.comment-body').first().fadeToggle({ duration: 0 });
-
-    //$(e).parent().find('.comment-body').each(function (index, item) {
-    //    $(item).show();
-    //});
-
-    if ($(e).find('.togglebutton').hasClass("fa-minus-square")) {
+    if (d === 1 || (d === 0 && $(e).find('.togglebutton').hasClass("fa-minus-square"))) {
         $(e).removeClass('pull-left');
         $(e).addClass('commentCollapsed');
         $(e).find('.togglebutton').removeClass("fa-minus-square");
         $(e).find('.togglebutton').addClass("fa-plus-square");
         $(e).find('#cel').show();
     }
-    else {
+    else if (d === -1 || (d === 0 && $(e).find('.togglebutton').hasClass("fa-plus-square"))) {
         $(e).addClass('pull-left');
         $(e).removeClass('commentCollapsed');
         $(e).find('.togglebutton').removeClass("fa-plus-square");
@@ -25,10 +26,12 @@ var toggleComment = function (e) {
 var togglePost = function (e) {
     $(e).parent().find('.social-body').slideToggle();
     if ($(e).find('.togglebutton').hasClass("fa-minus-square")) {
+        toggleComment($(e).parent().find('.social-comment-box').find('.comment-toggle'), 1);
         $(e).find('.togglebutton').removeClass("fa-minus-square");
         $(e).find('.togglebutton').addClass("fa-plus-square");
     }
     else {
+        toggleComment($(e).parent().find('.social-comment-box').find('.comment-toggle'), -1);
         $(e).find('.togglebutton').removeClass("fa-plus-square");
         $(e).find('.togglebutton').addClass("fa-minus-square");
     }
