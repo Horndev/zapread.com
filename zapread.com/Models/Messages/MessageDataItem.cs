72e84cee06bea2e7ae8521b95e69d9a35bd3919c
Steven Horn <horn@horndev.com>
2020-03-17 15:07:58 -0300

    Clean up GetMessagesTable.  Added message id to table return.

---
+++ zapread.com/Models/Messages/MessageDataItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace zapread.com.Models.Messages
+{
+    // used for The message table in /Messages/GetMessagesTable/
+    public class MessageDataItem
+    {
+        public int Id { get; set; }
+        public string Status { get; set; }
+        public string Type { get; set; }
+        public string From { get; set; }
+        public string FromID { get; set; }
+        public string Date { get; set; }
+        public string Link { get; set; }
+        public string Anchor { get; set; }
+        public string Message { get; set; }
+    }
+}
