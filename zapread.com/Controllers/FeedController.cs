ff69aad8363b8426452b3d28b554c140ebd4ef20
Steven Horn <horn@horndev.com>
2020-03-17 21:21:02 -0300

    Added Controller for /Feed

---
+++ zapread.com/Controllers/FeedController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace zapread.com.Controllers
+{
+    public class FeedController : Controller
+    {
+        // GET: Feed
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
