90d077350f4297bc3b7c519876818987e4897aa2
Steven Horn <horn@horndev.com>
2020-04-04 16:58:03 -0300

    Added some admin API functionality.

---
+++ zapread.com/Models/API/ZapReadResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace zapread.com.Models.API
+{
+    /// <summary>
+    /// API response
+    /// </summary>
+    public class ZapReadResponse
+    {
+        /// <summary>
+        /// True if the request completed successfully.
+        /// </summary>
+        public bool success { get; set; }
+
+        /// <summary>
+        /// Message from the sever (usually when not successful)
+        /// </summary>
+        public string message { get; set; }
+    }
+}
