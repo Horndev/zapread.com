186d0a2ad54ff1fcc7d2ae8701ba478e0a67a027
Steven Horn <horn@horndev.com>
2020-04-17 20:55:41 -0300

    Added realtime.zapread.com project which will host a SignalR Core proxy - which doesn't require jquery.

---
+++ realtime.zapread.com/Controllers/ChatController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace realtime.zapread.com.Controllers
+{
+    [ApiController]
+    //[Route("api/[controller]")]
+    public class ChatController : ControllerBase
+    {
+        private readonly ILogger<ChatController> _logger;
+
+        public ChatController(ILogger<ChatController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("api/[controller]")]
+        public string Get()
+        {
+            return "This endpoint manages zapread.com chats.";
+        }
+
+        [HttpPost]
+        [Route("api/[controller]/send")]
+        public string SendMessage(string message)
+        {
+            return message;
+        }
+    }
+}
