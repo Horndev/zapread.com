a322474a2751d8d60cec92690e50345cff01b14d
Steven Horn <horn@horndev.com>
2020-05-07 12:22:36 -0300

    Some minor updates -
    * style for comments
    * convert switchDeposit and switchWithdraw to not use jQuery

--- zapread.com/Scripts/src/pages/post/newpost.js
+++ zapread.com/Scripts/src/pages/post/newpost.js
@@ -4,15 +4,15 @@
 
 //import '../../shared/shared';
 
-import 'bootstrap';                                                         // still requires jquery :(
-import 'bootstrap/dist/css/bootstrap.min.css';                 // [✓]
-import 'font-awesome/css/font-awesome.min.css';                // [✓]
-import '../../utility/ui/paymentsscan';                        // [  ]
-import '../../utility/ui/accountpayments';                     // [  ]
-import '../../shared/postfunctions';                           // [✓]
-import '../../shared/readmore';                                // [✓]
-import '../../shared/postui';                                  // [✓]
-import '../../shared/topnavbar';                               // [✓]
+import 'bootstrap';                                             // [X]            // still requires jquery :(
+import 'bootstrap/dist/css/bootstrap.min.css';                  // [✓]
+import 'font-awesome/css/font-awesome.min.css';                 // [✓]
+import '../../utility/ui/paymentsscan';                         // [  ]
+import '../../utility/ui/accountpayments';                      // [  ]
+import '../../shared/postfunctions';                            // [✓]
+import '../../shared/readmore';                                 // [✓]
+import '../../shared/postui';                                   // [✓]
+import '../../shared/topnavbar';                                // [✓]
 import "jquery-ui-dist/jquery-ui";                              // [X]
 import "jquery-ui-dist/jquery-ui.min.css";                      // [X]
 
