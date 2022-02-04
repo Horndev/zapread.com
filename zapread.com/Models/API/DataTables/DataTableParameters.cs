5397769e525f434844f066976a0979ed06681d23
Steven Horn <horn@horndev.com>
2022-02-03 23:25:17 -0400

    Added filter to financial view for LN transactions

--- zapread.com/Models/API/DataTables/DataTableParameters.cs
+++ zapread.com/Models/API/DataTables/DataTableParameters.cs
@@ -5,14 +5,22 @@ using System.Web;
 
 namespace zapread.com.Models.API.DataTables
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class DataTableParameters
     {
+#pragma warning disable CA2227 // Collection properties should be read only
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
         public List<DataTableColumn> Columns { get; set; }
         public int Draw { get; set; }
         public int Length { get; set; }
         public List<DataOrder> Order { get; set; }
         public Search Search { get; set; }
         public int Start { get; set; }
+        public string Filter { get; set; }
+#pragma warning restore CA2227 // Collection properties should be read only
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
     }
 
     public class DataTableColumn
