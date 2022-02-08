22f80f12deb7af21e1279ce7471388c189a9e01a
Steven Horn <horn@horndev.com>
2022-02-08 11:27:43 -0400

    tidy

--- zapread.com/Controllers/ManageController.cs
+++ zapread.com/Controllers/ManageController.cs
@@ -1398,7 +1398,6 @@ namespace zapread.com.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult LinkLogin(string provider)
         {
-
             // Request a redirect to the external login provider to link a login for the current user
             return new AccountController.ChallengeResult(provider, Url.Action("LinkLoginCallback", "Manage"), User.Identity.GetUserId());
         }
