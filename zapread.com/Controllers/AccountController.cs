c4c3ba9b78bb86514f4a9399c7f3e752c215ae71
Steven Horn <horn@horndev.com>
2022-02-17 11:20:38 -0400

    Slow down captcha audio rate

--- zapread.com/Controllers/AccountController.cs
+++ zapread.com/Controllers/AccountController.cs
@@ -827,13 +827,26 @@ namespace zapread.com.Controllers
                 System.Configuration.ConfigurationManager.AppSettings["SpeechServicesKey"],
                 System.Configuration.ConfigurationManager.AppSettings["SpeechServicesRegion"]);
 
+            var voiceName = "en-CA-LiamNeural";
             config.SpeechSynthesisLanguage = "en-CA";
-            config.SpeechSynthesisVoiceName = "en-CA-LiamNeural";
+            config.SpeechSynthesisVoiceName = voiceName;
             config.SetSpeechSynthesisOutputFormat(SpeechSynthesisOutputFormat.Audio16Khz32KBitRateMonoMp3);
+
+            // insert spaces between characters
+            var captchaCodeSpaced = String.Join(" ", captchaCode.ToCharArray());
 
             using (var synthesizer = new SpeechSynthesizer(config, null))
             {
-                var result = await synthesizer.SpeakTextAsync(captchaCode);
+                string ssml =   "<speak version=\"1.0\" xmlns=\"http://www.w3.org/2001/10/synthesis\" xml:lang=\"en-US\">" +
+                                "  <voice name=\"" + voiceName + "\">" +
+                                "    <prosody rate=\"-40.00%\">" +
+                                "      <say-as interpret-as=\"characters\">" +
+                                            captchaCodeSpaced +
+                                "      </say-as>" +
+                                "    </prosody>" +
+                                "  </voice>" +
+                                "</speak>";
+                var result = await synthesizer.SpeakSsmlAsync(ssml);
                 return File(result.AudioData, "audio/mpeg");
             }
         }
