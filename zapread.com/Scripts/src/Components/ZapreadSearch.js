245bd6c9e05c047281ebd5e76064744c836d4f55
Steven Horn <horn@horndev.com>
2022-03-11 13:41:30 -0400

    Added beta search capability

--- zapread.com/Scripts/src/Components/ZapreadSearch.js
+++ zapread.com/Scripts/src/Components/ZapreadSearch.js
@@ -6,14 +6,32 @@ import "../css/components/zrsearch.css";
 import React, { useCallback, useEffect, useState, createRef } from "react";
 import { InputGroup, FormControl, DropdownButton, Dropdown, Modal, Container, Row, Col, Button, Card } from "react-bootstrap";
 import ZapreadSearchResultModal from "./ZapreadSearchResultModal";
+import { getJson } from '../utility/getData';
+const getSwal = () => import('sweetalert2');
 
 export default function ZapreadSearch(props) {
   const [query, setQuery] = useState("");
   const [searchType, setSearchType] = useState("all");
   const [showSearch, setShowSearch] = useState(false);
-  const [results, setResults] = useState(["Coming soon!"]);
+  const [noResults, setNoResults] = useState(false);
+  const [results, setResults] = useState([]);
 
   const handleSearch = () => {
+    getJson('/api/v1/search/'+ searchType +'/?q=' + query).then((response) => {
+      if (response.success) {
+        if (response.result == 0) {
+          setNoResults(true);
+        }
+        else {
+          setResults(response.result);
+        }       
+      } else {
+        // Did not work
+        getSwal().then(({ default: Swal }) => {
+          Swal.fire("Error", "Error loading search: " + response.message, "error");
+        });
+      }
+    });
     setShowSearch(true);
   };
 
@@ -23,7 +41,7 @@ export default function ZapreadSearch(props) {
 
   return (
     <>
-      <ZapreadSearchResultModal show={showSearch} onClose={onCloseModal} query={query} results={results} />
+      <ZapreadSearchResultModal noResults={noResults} show={showSearch} onClose={onCloseModal} query={query} results={results} />
       <div className="ZRSearch">
         <InputGroup>
           <FormControl
@@ -31,6 +49,11 @@ export default function ZapreadSearch(props) {
             aria-label="Search"
             value={query}
             onChange={({ target: { value } }) => setQuery(value)} // Controlled input
+            onKeyPress={event => {
+              if (event.key === 'Enter') {
+                handleSearch();
+              }
+            }}
           />
           <InputGroup.Append>
             <Button variant="outline-primary" onClick={ handleSearch }>
@@ -39,22 +62,22 @@ export default function ZapreadSearch(props) {
               <i className="fa-solid fa-magnifying-glass" style={searchType == "all" ? {} : { display: "none" }}></i>
             </Button>
           </InputGroup.Append>
-          <DropdownButton
-            as={InputGroup.Append}
-            variant="outline-primary"
-            title=""
-            id="input-group-dropdown-search"
-          >
-            <Dropdown.Item href="#" onClick={() => setSearchType("all")}>
-              {searchType == "all" ? <><i className="fa-solid fa-check"></i></> : <></>}{" "}Find Anything
-            </Dropdown.Item>
-            <Dropdown.Item href="#" onClick={() => setSearchType("post")}>
-              {searchType == "post" ? <><i className="fa-solid fa-check"></i></> : <></>}{" "}Find Posts
-            </Dropdown.Item>
-            <Dropdown.Item href="#" onClick={() => setSearchType("user")}>
-              {searchType == "user" ? <><i className="fa-solid fa-check"></i></> : <></>}{" "}Find Users
-            </Dropdown.Item>
-          </DropdownButton>
+          {/*<DropdownButton*/}
+          {/*  as={InputGroup.Append}*/}
+          {/*  variant="outline-primary"*/}
+          {/*  title=""*/}
+          {/*  id="input-group-dropdown-search"*/}
+          {/*>*/}
+          {/*  <Dropdown.Item href="#" onClick={() => setSearchType("all")}>*/}
+          {/*    {searchType == "all" ? <><i className="fa-solid fa-check"></i></> : <></>}{" "}Find Anything*/}
+          {/*  </Dropdown.Item>*/}
+          {/*  <Dropdown.Item href="#" onClick={() => setSearchType("post")}>*/}
+          {/*    {searchType == "post" ? <><i className="fa-solid fa-check"></i></> : <></>}{" "}Find Posts*/}
+          {/*  </Dropdown.Item>*/}
+          {/*  <Dropdown.Item href="#" onClick={() => setSearchType("user")}>*/}
+          {/*    {searchType == "user" ? <><i className="fa-solid fa-check"></i></> : <></>}{" "}Find Users*/}
+          {/*  </Dropdown.Item>*/}
+          {/*</DropdownButton>*/}
         </InputGroup>
       </div>
     </>
