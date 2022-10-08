e291e1ff3e089cc8ab734afba5712c08e04c8002
Finn Westendorf <42862612+wfinn@users.noreply.github.com>
2022-10-09 01:19:15 +0200

    URL check in onusermessage

--- zapread.com/Scripts/src/realtime/notification/onusermessage.js
+++ zapread.com/Scripts/src/realtime/notification/onusermessage.js
@@ -8,6 +8,8 @@ import * as tata from 'tata-js'
 export function onusermessage(content, reason, clickUrl) {
   var cleanContent = DOMPurify.sanitize(content);
   var cleanReason = DOMPurify.sanitize(reason);
+  if(/^(https?:\/\/|\/)/.test(clickUrl))
+    clickUrl = "https://" + clickUrl;
   tata.info(cleanReason, cleanContent, {
     position: "br",
     duration: 10000,
